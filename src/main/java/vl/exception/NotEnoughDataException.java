95fb8651ee52bfaf3152a28a6c0d5541b97ef881
VL <vlisianskii@gmail.com>
2020-06-06 14:16:46 -0400

    Removed exception's fields

--- src/main/java/vl/exception/NotEnoughDataException.java
+++ src/main/java/vl/exception/NotEnoughDataException.java
@@ -1,13 +1,7 @@
 package vl.exception;
 
-import lombok.Getter;
-
-@Getter
 public class NotEnoughDataException extends RuntimeException {
-    private final Object y;
-
-    public NotEnoughDataException(Object x, Object y) {
-        super(String.format("Not enough input data by x='%s' y='%s'", x, y));
-        this.y = y;
+    public NotEnoughDataException(String msg) {
+        super(msg);
     }
 }
